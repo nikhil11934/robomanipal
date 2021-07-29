55a6b8abe5413bf1265d4cd8b926fb3dae1866ca
Ujjwal Sharma <82819475+ujjwalsharma-08@users.noreply.github.com>
2021-07-29 15:21:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// sticky navbar on scrolling
 window.addEventListener("scroll", function()
 {
     if(window.scrollY > 10)
@@ -27,56 +26,3 @@ function menutoggle()
        
 }
 
-
-function onButtonClick() {
-    document.getElementById("features").scrollIntoView();
-  }
-
-
-
-
-
-
-
-
-// to change imgs according to the colors
-
-function showc1()
-{
-    document.getElementById('diffcolors').src="https://imgur.com/hg9SG3g.png";
-}
-
-function showc2()
-{
-    document.getElementById('diffcolors').src="https://imgur.com/TM7PeXd.png";
-}
-
-function showc3()
-{
-    document.getElementById('diffcolors').src="https://imgur.com/Rw5d77x.png";
-}
-
-function showc4()
-{
-    document.getElementById('diffcolors').src="https://imgur.com/Qzor5fb.png";
-}
-
-function showc5()
-{
-    document.getElementById('diffcolors').src="https://imgur.com/mUnM3IH.png";
-}
-
-function showc6()
-{
-    document.getElementById('diffcolors').src="https://imgur.com/x7YSWB1.png";
-}
-
-function showc7()
-{
-    document.getElementById('diffcolors').src="https://imgur.com/UrpJE7Z.png";
-}
-
-function showc8()
-{
-    document.getElementById('diffcolors').src="https://imgur.com/NhNLYpm.png";
-}
